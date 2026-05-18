b14b1698923eaa22f556fad963b4b9da9d996910
xDk <linuxkali717@gmail.com>
2026-05-18 10:09:28 +0300

    ADMINS fixed

--- src/app/api/getAllUsers/route.ts
+++ src/app/api/getAllUsers/route.ts
@@ -1,4 +1,5 @@
 import { clerkClient } from "@clerk/nextjs/server";
+import { ADMINS } from "@/lib/utils";
 
 export async function GET() {
   try {
@@ -7,14 +8,16 @@ export async function GET() {
     const users = await clerk.users.getUserList();
 
     return Response.json(
-      users.data.map((u) => ({
-        id: u.id,
-        username:
-          u.username ||
-          `${u.firstName || ""} ${u.lastName || ""}`.trim() ||
-          u.primaryEmailAddress?.emailAddress,
-        image: u.imageUrl,
-      })),
+      users.data
+        .filter((u) => !ADMINS.includes(u.id))
+        .map((u) => ({
+          id: u.id,
+          username:
+            u.username ||
+            `${u.firstName || ""} ${u.lastName || ""}`.trim() ||
+            u.primaryEmailAddress?.emailAddress,
+          image: u.imageUrl,
+        })),
     );
   } catch (error) {
     console.error("CLERK ERROR:", error);
