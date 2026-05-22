283cbfa9440cc47d93de9f5ed52589c353433afd
x <x>
2026-05-22 20:55:41 +0300

    added link edit in [id] route

--- src/app/(main)/transactions/[id]/page.tsx
+++ src/app/(main)/transactions/[id]/page.tsx
@@ -161,7 +161,7 @@ export default function TransactionPage() {
 		placeholder="Paste Reciept Link"
 		value={url}
 		onChange={(e) => setUrl(e.target.value)}
-		className="outline-none text-xs rounded-full p-3 border-transparent hover:border-gray-700 border"
+		className="outline-none text-xs rounded-full p-3 px-5 border-gray-700/60 focus:border-gray-700 hover:border-gray-700 border"
 	      />
 	    }
             </div>
