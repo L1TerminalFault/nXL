cd90d807fad21595f254fdac12a44b309ceff1e7
xDk <linuxkali717@gmail.com>
2026-06-10 12:33:48 +0300

    password field changes

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="md:p-10 p-3 pt-6 gap-8 h-full min-h-screen items-center justify-center/ w-full flex flex-col">
       <div className="flex w-full text-lg flex-1 text-center py-10 gap-6 h-full items-center flex-col">
-        Feature is not implemented. </br > Stay tuned for updates.
+        Feature is not implemented. <br /> Stay tuned for updates.
       </div>
     </div>
   );
